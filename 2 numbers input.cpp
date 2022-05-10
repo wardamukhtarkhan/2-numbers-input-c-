65ae8f3443aec05c865b13e708497b45a77261fb
wardamukhtarkhan <102410054+wardamukhtarkhan@users.noreply.github.com>
2022-05-10 18:19:44 +0500

    Add files via upload

---
+++ 2 numbers input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    
+    cout << "Hello world!"<<endl;
+
+   cout<<"enter two number: ";
+   cin>>num1>>num2;
+   cout<<"the first number is:"<<num1<<endl;
+   cout<<"the second number is:"<<num2<<endl;
+   
+    return 0;
+}
